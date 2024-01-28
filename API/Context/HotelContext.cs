d515cfc00194fc8e82e5865b66978ed674246112
Chase <chase.seltz21@gmail.com>
2024-01-28 12:02:32 -0700

    Create DbContext

---
+++ API/Context/HotelContext.cs
@@ -0,0 +1,19 @@
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Context;
+
+public class HotelContext : DbContext
+{
+    public HotelContext(DbContextOptions<HotelContext> contextOptions) : base(contextOptions) {}
+
+    public DbSet<Guest> Guests { get; set; }
+    public DbSet<PaymentStatus> PaymentStatus { get; set; }
+    public DbSet<Booking> Bookings { get; set; }
+    public DbSet<Room> Rooms { get; set; }
+    public DbSet<Floor> Floors { get; set; }
+    public DbSet<RoomClass> RoomClasses { get; set; }
+    public DbSet<RoomStatus> RoomStatus { get; set; }
+    public DbSet<Bed> Beds { get; set; }
+    public DbSet<Feature> Features { get; set; }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,21 @@
+using System.Text.Json.Serialization;
+using API.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        // Lets you include navigation properties, ignoring cycle issues
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    });
+builder.Services.AddDbContext<HotelContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();

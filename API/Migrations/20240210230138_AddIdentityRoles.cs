d925ecb310b1241e615a5052755c2464967c418a
Chase <chase.seltz21@gmail.com>
2024-02-10 16:02:42 -0700

    Create AddIdentityRoles migration

---
+++ API/Migrations/20240210230138_AddIdentityRoles.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIdentityRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "5a95b437-2f00-403d-810a-6164296b0c1d", null, "Manager", "MANAGER" },
+                    { "62f68206-56f5-4944-8ab0-ef61b5e9727e", null, "Employee", "EMPLOYEE" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "5a95b437-2f00-403d-810a-6164296b0c1d");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "62f68206-56f5-4944-8ab0-ef61b5e9727e");
+        }
+    }
+}
